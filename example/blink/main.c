02df23838ccc91e16fe1477cd617aa79b678fb69
Brad Campbell <bradjc5@gmail.com>
2015-07-10 14:24:53 -0400

    update blink and makefile

--- example/blink/main.c
+++ example/blink/main.c
@@ -1,7 +1,7 @@
 #include "stm32f0xx_gpio.h"
 #include "stm32f0xx_rcc.h"
 
-GPIO_InitTypeDef 	GPIO_InitStructure;
+GPIO_InitTypeDef  GPIO_InitStructure;
 
 /// Spin delay
 void delay(int count)
@@ -18,28 +18,39 @@ void delay(int count)
 int main(void)
 {
 
-	/* GPIOC Periph clock enable */
-  	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+  /* GPIOC Periph clock enable */
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
 
-  	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
-  	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  	GPIO_Init(GPIOC, &GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
 
 
     for (;;)
     {
-		//turn on LED
-		GPIOC->BSRR = 0xFFFF;		
+      // Turn on both LEDs
+      GPIOC->BSRR = 0x0300;
 
-        // Around 1/4 of a second
-        delay(2400000);
-	
-		//turn off LED
-		GPIOC->BRR = 0xFFFF;
+      // Around 1/4 of a second
+      delay(2400000);
 
-        delay(2400000);
+      // Turn off the first led
+      GPIOC->BRR = 0x0100;
+
+      delay(2400000);
+
+      // Turn off the second LED and the on the first
+      GPIOC->BRR = 0x0200;
+      GPIOC->BSRR = 0x0100;
+
+      delay(2400000);
+
+      // Turn off first
+      GPIOC->BRR = 0x0100;
+
+      delay(2400000);
     }
 }
